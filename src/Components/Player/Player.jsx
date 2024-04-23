2554331072333f1c4d8d87d2a6022cc1fa847913
XXX <muhiddinovhusan0207@gmail.com>
2024-04-23 21:01:35 +0500

    x

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,15 +1,19 @@
 import { loadFromLocalStorage } from '../../app/like/LikeSlice';
-import './Player.scss'
+import './Player.scss';
+
 const Player = () => {
   const Track = loadFromLocalStorage("selectedTrack");
-  console.log(Track)
+  console.log(Track);
 
   return (
-    
-    <div className='Player  flex justify-center items-center'>
-      <audio  className='w-full' controls src={Track.track.preview_url}></audio>
-       </div>
-  )
-}
+    <div className='Player flex justify-center items-center'>
+      {Track ? (
+        <audio className='w-full' controls src={Track.track.preview_url}></audio>
+      ) : (
+        <p>No track selected</p>
+      )}
+    </div>
+  );
+};
 
-export default Player
+export default Player;
