dfe37ba00c56d8b7b78972d14dfeb984aa01193e
XXX <muhiddinovhusan0207@gmail.com>
2024-06-07 23:14:11 +0500

    x

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -16,41 +16,70 @@ const Footer = () => {
   }
   const PlayerContent = useSelector(state => state.like.PlayerContent)
 
-const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  const handleLinkClick1 = (event) => {
+    event.preventDefault();
+    if (PlayerContent) {
+      closePlayerContent();
+      navigate('/')
+    }
+    else {
+      navigate('/')
+    }
+
+  }
   const handleLinkClick = (event) => {
-    event.preventDefault(); 
-    closePlayerContent(); 
-    navigate('/')
+    event.preventDefault();
+    if (PlayerContent) {
+      closePlayerContent();
+      navigate('/likedSongs')
+    }
+    else {
+      navigate('/likedSongs')
+    }
+
+  }
+  const handleLinkClick2 = (event) => {
+    event.preventDefault();
+    if (PlayerContent) {
+      closePlayerContent();
+      navigate('https://open.spotify.com/')
+    }
+    else {
+      navigate('https://open.spotify.com/')
+    }
+
   }
 
   return (
     <div className='fixed md:hidden h-16 bg-[#000]  w-full pt-1 -bottom-1 grid grid-cols-5 z-50  '>
-      <div  onClick={PlayerContent? handleLinkClick :''}   className=' pl-1  text-[11px]'>
+      <div onClick={handleLinkClick1} className=' pl-1  text-[11px]'>
 
         <img className='pl-2 p-1 ' src={homeicon} alt="" />
 
         <p className='pl-1  font-semibold text-[#b3b3ad]'>Главная</p>
       </div>
-      <Link onClick={PlayerContent? handleLinkClick :''}  to='' className='w-20 text-[12px]'>
+      <div onClick={handleLinkClick1} to='' className='w-20 text-[12px]'>
         <img className='pl-2 p-1' src={searchicon} alt="" />
         <p className='ml-1  font-semibold  text-[#b3b3ad]'>Поиск</p>
-      </Link>
-      <Link className='w-[100px] text-[11px]'>
+      </div>
+      <div className='w-[100px] cursor-pointer text-[11px]'>
         <img className='pl-6 p-0.5' src={libraryicon} alt="" />
 
         <p className='pt-1   font-semibold text-[#b3b3ad]'>Моя медиатека</p>
-      </Link>
-      <Link onClick={PlayerContent? handleLinkClick :''}  to='https://open.spotify.com/' className='w-full  pl-6 text-[11px] '>
+      </div>
+      <Link to='https://open.spotify.com'  className='w-full  pl-6 text-[11px] '>
         <div className='flex  items-center'>
 
           <img className='w-10 ' src={spotify} alt="" />
         </div>
         <p className=' font-semibold text-[#b3b3ad]'>Spotify</p>
       </Link>
-      <Link onClick={PlayerContent? handleLinkClick :''}  to='/likedSongs' className='p-2'>
+      <div onClick={ handleLinkClick } className='p-2'>
         <img src={LikeIcon} alt="" className='ml-4' />
         <h2 className='text-[#b3b3ad] text-xs'>Liked Songs</h2>
-      </Link>
+      </div>
     </div>
 
   )
